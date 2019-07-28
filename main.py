88e6cc7b54bd73a9b9e6f9d0ef7c4b0cb0aa11c0
Alexey Kovalenko <epfly@ya.ru>
2019-07-28 03:31:22 +0300

    add toggle admin function

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+#!/usr/bin/python3
+
 from vk_http import VkHttp
 
 bot = VkHttp('login', 'password')
+group_id = 151378510
 chat_id = bot.create_chat(user_ids=[187552130], title='test')
-bot.add_group_to_chat(chat_id=chat_id, group_id=151378510)
+bot.add_group_to_chat(chat_id=chat_id, group_id=group_id)
+bot.toggle_admin(chat_id=chat_id, group_id=group_id)
--- vk_http.py
+++ vk_http.py
@@ -20,6 +20,29 @@ class VkHttp:
 			data = {'email': login, 'pass': password}
 		)
 
+	def create_chat(self, user_ids, title):
+		return self._create_chat_by_hash(user_ids, title, self._get_write_hash())
+
+	def _get_create_chat_html(self):
+		return self.session.get('https://vk.com/im?act=create')
+
+	def _get_write_hash(self):
+		return re.search(r'(?:"writeHash":")(.*?)(?:")', self._get_create_chat_html().text).group(1)
+
+	def _create_chat_by_hash(self, user_ids, title, hash):
+		result = self.session.post(
+			'https://vk.com/al_im.php',
+			data = {
+				'al': 1,
+				'im_v': 2,
+				'hash': hash,
+				'act': 'a_multi_start',
+				'peers': ','.join(str(x) for x in user_ids),
+				'title': title
+			}
+		).text
+		return re.search(r'(?:"peerId":)(\d+)', result).group(1)
+
 	def add_group_to_chat(self, chat_id, group_id):
 		return self._invite_by_hash(chat_id, group_id, self._get_inviting_hash(group_id))
 
@@ -27,8 +50,8 @@ class VkHttp:
 		return self.session.post(
 			'https://vk.com/al_groups.php',
 			data = {
-				'act': 'a_search_chats_box',
 				'al': 1,
+				'act': 'a_search_chats_box',
 				'group_id': group_id
 			}
 		)
@@ -48,25 +71,20 @@ class VkHttp:
 			}
 		).text
 
-	def create_chat(self, user_ids, title):
-		return self._create_chat_by_hash(user_ids, title, self._get_write_hash())
+	def toggle_admin(self, chat_id, group_id):
 
-	def _get_create_chat_html(self):
-		return self.session.get('https://vk.com/im?act=create')
+		chat_page = self.session.get('https://vk.com/im?sel=c%d' % (int(chat_id) - 2000000000)).text
+		hash = re.search(r'(?:dialog&hash=)(.*?)(?:&)', chat_page).group(1)
 
-	def _get_write_hash(self):
-		return re.search(r'(?:"writeHash":")(.*?)(?:")', self._get_create_chat_html().text).group(1)
-
-	def _create_chat_by_hash(self, user_ids, title, hash):
-		result = self.session.post(
+		self.session.post(
 			'https://vk.com/al_im.php',
 			data = {
-				'act': 'a_multi_start',
 				'al': 1,
 				'im_v': 2,
-				'peers': ','.join(str(x) for x in user_ids),
-				'title': title,
-				'hash': hash
+				'hash': hash,
+				'act': 'a_toggle_admin',
+				'is_admin': 1,
+				'chat': chat_id,
+				'mid': -group_id
 			}
-		).text
-		return re.search(r'(?:"peerId":)(\d+)', result).group(1)
+		)
